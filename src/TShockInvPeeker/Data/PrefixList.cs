a7ff4016cbfdaedb31b030a56b662730ba312403
Kassab <CKassabAndre@gmail.com>
2020-04-29 16:39:49 -0500

    Added CSV's and a new class for it

---
+++ src/TShockInvPeeker/Data/PrefixList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TShockInvPeeker.Data
+{
+    public class PrefixList
+    {
+        public string PrefixName { get; set; }
+        public byte PrefixID { get; set; }
+
+    }
+}
