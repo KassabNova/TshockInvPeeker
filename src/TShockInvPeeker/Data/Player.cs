bf5e1a17ee389268c3cde67b32b013bb8dcb0ef0
Kassab <CKassabAndre@gmail.com>
2020-04-28 23:19:23 -0500

    Classes for future growth

---
+++ src/TShockInvPeeker/Data/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TShockInvPeeker.Data
+{
+    public class Player
+    {
+        public byte ID { get; set; }
+        public string Username { get; set; }
+        public string Group { get; set; }
+        public short MaxHP { get; set; }
+        public short MaxMana { get; set; }
+        public string Inventory { get; set; }
+    }
+}
