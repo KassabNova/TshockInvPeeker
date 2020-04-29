e3e34ba6546ca7ed473ca3d4182f6e022e40a93d
Kassab <CKassabAndre@gmail.com>
2020-04-28 23:21:41 -0500

    Added base classes

---
+++ src/TShockInvPeeker/Data/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TShockInvPeeker.Data
+{
+    public class Item
+    {
+        public ushort itemID { get; set; }
+        public byte prefixID { get; set; }
+        public string itemName { get; set; }
+        public string prefixName { get; set; }
+        public ushort ammount { get; set; }
+
+    }
+}
---
+++ src/TShockInvPeeker/Data/ItemList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TShockInvPeeker.Data
+{
+    public class ItemList
+    {
+        public string ItemName { get; set; }
+        public ushort ItemID { get; set; }
+
+    }
+}
